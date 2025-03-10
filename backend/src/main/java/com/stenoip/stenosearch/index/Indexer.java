f3febca7dc850f07fbb512ba3fb98e0a3a3ae118
StenoipWonderComputer <mystenoipcompany@gmail.com>
2025-03-10 16:30:21 -0400

    Create Indexer.java

---
+++ backend/src/main/java/com/stenoip/stenosearch/index/Indexer.java
@@ -0,0 +1,31 @@
+package com.stenoip.stenosearch.index;
+
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import java.io.IOException;
+import java.util.Map;
+
+public class Indexer {
+    private RestHighLevelClient client;
+
+    public Indexer(RestHighLevelClient client) {
+        this.client = client;
+    }
+
+    public void createIndex(String indexName, Map<String, Object> document) {
+        try {
+            // Create an index request
+            IndexRequest request = new IndexRequest(indexName);
+            request.source(document, XContentType.JSON);
+
+            // Index the document
+            IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+            System.out.println("Indexed document ID: " + response.getId());
+        } catch (IOException e) {
+            System.err.println("Error indexing document: " + e.getMessage());
+        }
+    }
+}
